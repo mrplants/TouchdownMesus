e7b86bbc327f925157faa65b0b90a1d6f0c84a74
Sean T Fitzgerald <fitzgerald.sean.t@gmail.com>
2013-05-24 06:34:24 -0400

    Initial Commit

---
+++ Touchdown Me-sus/TDMAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  TDMAppDelegate.h
+//  Touchdown Me-sus
+//
+//  Created by Sean Fitzgerald on 5/24/13.
+//  Copyright (c) 2013 Sean T Fitzgerald. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TDMAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Touchdown Me-sus/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Touchdown Me-sus
+//
+//  Created by Sean Fitzgerald on 5/24/13.
+//  Copyright (c) 2013 Sean T Fitzgerald. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "TDMAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([TDMAppDelegate class]));
+	}
+}
